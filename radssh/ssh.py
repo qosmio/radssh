43572d5b3a30500549466afe830fbd447a46fa9c
Mark Kelly <mark.kelly@lexisnexisrisk.com>
2020-03-06 11:54:20 -0500

    Updates

--- radssh/ssh.py
+++ radssh/ssh.py
@@ -406,8 +406,11 @@ def sftp_thread(host, t, srcfile, dstfile=None, attrs=None):
     if not dstfile:
         dstfile = srcfile
     s.put(srcfile, dstfile)
-    s.chown(dstfile, attrs.st_uid, attrs.st_gid)
     s.chmod(dstfile, attrs.st_mode % 4096)
+    try:
+        s.chown(dstfile, attrs.st_uid, attrs.st_gid)
+    except IOError:
+        pass
     s.close()
     return CommandResult(command='SFTP %s -> %s' % (srcfile, dstfile),
                          return_code=0, status='*** Complete ***',
@@ -448,7 +451,7 @@ class Cluster(object):
         self.log_out = self.defaults.get('log_out', 'out.log').strip()
         self.log_err = self.defaults.get('log_err', 'err.log').strip()
         thread_count = min(int(self.defaults.get('max_threads')), len(hostlist))
-        self.dispatcher = Dispatcher(outQ=queue.Queue(), threadpool_size=thread_count, dynamic_expansion=True)
+        self.dispatcher = Dispatcher(outQ=queue.Queue(), threadpool_size=thread_count)
         self.pending = {}
         self.uuid = uuid.uuid1()
         self.connections = {}
